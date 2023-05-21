e1c8e87472387e31399afda1df08ee4573785926
muskbuster <sudeepkamat79@gmail.com>
2023-05-21 11:22:29 +0000

    start today bruh

---
+++ src/guessing_game/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use rand::distributions::Uniform;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+    let secret= rand::
+    thread_rng()
+    .sample(Uniform::new(25u32, 150));
+loop{
+    let mut guess = String::new();
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
+
+  
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {println!("You win!");
+        break;  }
+
+    }
+
+
+    println!("You guessed: {guess}");
+}
+}
